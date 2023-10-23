3256411afd4448e71550ed11b1be43efe1ea1d15
nyamekyekutortse <nyamekye@kutortse.com>
2023-10-23 19:45:14 -0400

    update

--- profiles/producer/javascript/chatFeature.js
+++ profiles/producer/javascript/chatFeature.js
@@ -20,4 +20,31 @@ sendMessageButton.addEventListener("click", function()
     newOutgoingMessage.children[1].innerHTML = messageInput.value;
     allMessages.insertAdjacentElement("beforeend", newOutgoingMessage);
     messageInput.value = "";
+    allMessages.scrollTop = allMessages.scrollHeight;
 });
+
+messageInput.addEventListener("keydown", function(event)
+{
+    if(event.key === "Enter")
+    {
+        const timestamp = currentTime();
+        var newOutgoingMessage = outgoingMessageTemplate.cloneNode(true);
+        console.log(timestamp);
+        console.log(outgoingMessageTemplate);
+        console.log(allMessages);
+        if(timestamp[3] === 1)
+            newOutgoingMessage.children[0].innerHTML = timestamp[0] + ":" + timestamp[1] + "<sub>AM</sub>";
+        else
+            newOutgoingMessage.children[0].innerHTML = timestamp[0] + ":" + timestamp[1] + "<sub>PM</sub>";
+        newOutgoingMessage.children[1].innerHTML = messageInput.value;
+        allMessages.insertAdjacentElement("beforeend", newOutgoingMessage);
+        messageInput.value = "";
+        allMessages.scrollTop = allMessages.scrollHeight;
+    }
+})
+
+window.addEventListener("load", function()
+{
+    // scroll chat to most recent
+    allMessages.scrollTop = allMessages.scrollHeight;
+})
