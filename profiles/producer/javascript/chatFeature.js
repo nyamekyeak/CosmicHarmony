fe746da03a10bbee6611305f2e0f487c6794d5d9
nyamekyekutortse <nyamekye@kutortse.com>
2023-10-23 14:52:42 -0400

    updates

---
+++ profiles/producer/javascript/chatFeature.js
@@ -0,0 +1,23 @@
+const messageInput = document.getElementById("newMessageInput");
+const chatContainer = document.getElementById("chatScreen");
+const allMessages = document.getElementById("messages");
+const sendMessageButton = document.getElementById("sendMessage");
+const incomingMessages = document.getElementsByClassName("incomingMessage");
+const outgoingMessages = document.getElementsByClassName("outgoingMessage");
+const outgoingMessageTemplate = outgoingMessages[0].cloneNode(true);
+
+sendMessageButton.addEventListener("click", function()
+{
+    const timestamp = currentTime();
+    var newOutgoingMessage = outgoingMessageTemplate.cloneNode(true);
+    console.log(timestamp);
+    console.log(outgoingMessageTemplate);
+    console.log(allMessages);
+    if(timestamp[3] === 1)
+        newOutgoingMessage.children[0].innerHTML = timestamp[0] + ":" + timestamp[1] + "<sub>AM</sub>";
+    else
+        newOutgoingMessage.children[0].innerHTML = timestamp[0] + ":" + timestamp[1] + "<sub>PM</sub>";
+    newOutgoingMessage.children[1].innerHTML = messageInput.value;
+    allMessages.insertAdjacentElement("beforeend", newOutgoingMessage);
+    messageInput.value = "";
+});
